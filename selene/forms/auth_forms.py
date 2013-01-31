a76d23f0f95e460b410f3d95b96aaea79d5d022f
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-01-30 23:30:56 -0500

    Building register form with WTForms

---
+++ selene/forms/auth_forms.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 *-*
+from wtforms import Form, TextField, PasswordField
+from wtforms.validators import Required, Email
+
+
+class RegisterForm(Form):
+
+    name = TextField(u"Full name", [Required()])
+    email = TextField(u"E-mail", [Required(), Email()])
+    password = PasswordField(u"Password", [Required()])
