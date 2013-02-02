c7183acecb50bed98a54bb29fc81f8f290825545
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-02-02 00:48:03 -0500

    Building edit/new post form using WTForms

--- selene/forms/blog_forms.py
+++ selene/forms/blog_forms.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 *-*
-from wtforms import Form, TextField, TextAreaField
+from wtforms import Form, TextField, BooleanField, TextAreaField, RadioField
 from wtforms.validators import Required
 
 
 class NewPostForm(Form):
 
     title = TextField(u'Title', [Required()])
-    custom_slug = TextField(u'Custom slug', [Required()])
+    slug = BooleanField(u'Custom slug', )
+    customslug = TextField()
     tags = TextField(u'Tags', [Required()])
     content = TextAreaField(u'Content', [Required()])
+    status = RadioField(u'Status', [Required()],
+        choices=[('published', 'Published'), ('unpublished', 'Unpublished')])
