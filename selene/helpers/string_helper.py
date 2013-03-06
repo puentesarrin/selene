79e28cc17aa6016decc813edb3a4f23c80c94957
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-03-05 23:44:49 -0500

    Getting string only from html for search posts

--- selene/helpers/string_helper.py
+++ selene/helpers/string_helper.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 *-*
 import re
+
+from HTMLParser import HTMLParser
 from unicodedata import normalize
 
 _punct_re = re.compile(r'[\t !"#$%&\'()*\:\;\-/<=>?@\[\\\]^_`{|},.]+')
@@ -12,3 +14,30 @@ def get_slug(input_text, delim=u"-"):
         if word:
             result.append(word)
     return unicode(delim.join(result))
+
+whitespace = re.compile('\s+')
+
+
+class HTMLStripTags(HTMLParser):
+
+    def __init__(self, *args, **kwargs):
+        HTMLParser.__init__(self, *args, **kwargs)
+        self.out = ""
+
+    def handle_data(self, data):
+        self.out += data
+
+    def handle_entityref(self, name):
+        self.out += '&%s;' % name
+
+    def handle_charref(self, name):
+        return self.handle_entityref('#' + name)
+
+    def value(self):
+        return whitespace.sub(' ', self.out).strip()
+
+
+def get_plain(html):
+    parser = HTMLStripTags()
+    parser.feed(html)
+    return parser.value()
