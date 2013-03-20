df34ab5a4ae84379ee1f00282abcb4cad9d99102
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-03-20 15:17:52 -0500

    Logging at run

--- run.py
+++ run.py
@@ -11,8 +11,11 @@ from selene import options, handlers, Selene
 if __name__ == '__main__':
     options.setup_options('selene.conf')
     db = pymongo.MongoClient(opts.db_uri)[opts.db_name]
+    logging.info('Connected to MongoDB.')
     tornado.locale.load_translations("translations")
     tornado.locale.set_default_locale(opts.default_locale)
+    logging.info('Loaded translations: %s.' %
+        ', '.join(tornado.locale.get_supported_locales()))
     http_server = tornado.httpserver.HTTPServer(Selene(db))
     tornado.web.ErrorHandler = handlers.ErrorHandler
     http_server.listen(opts.port)
