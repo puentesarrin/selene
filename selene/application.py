3c5b2129c659e0200a23d217f73f1937d19e3f0e
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-03-20 21:03:01 -0500

    Fix db_use_fts validation

--- selene/application.py
+++ selene/application.py
@@ -21,17 +21,18 @@ class Selene(tornado.web.Application):
             'cookie_secret': opts.cookie_secret,
             'ui_modules': ui_modules
         }
-        opts.db_use_fts = (opts.db_use_fts and
-            'text' in db.command("listCommands")['commands'] and
-            db.connection.admin.command('getParameter', 1,
-                textSearchEnabled=1)['textSearchEnabled'])
-        if not opts.db_use_fts:
-            logging.warning('Full text search is probably not activated on '
-                'MongoDB server, If you want to activated it, use 2.4 version '
-                'and issue the following command on admin database:\n'
-                'db.runCommand({ setParameter: 1, textSearchEnabled: true })')
-        else:
-            db.posts.ensure_index([('plain_content', 'text')])
+        if opts.db_use_fts:
+            opts.db_use_fts = ('text' in db.command("listCommands")['commands']
+                and db.connection.admin.command('getParameter', 1,
+                    textSearchEnabled=1)['textSearchEnabled'])
+            if not opts.db_use_fts:
+                logging.warning('Full text search is probably not activated '
+                    'on MongoDB server, If you want to activated it, use 2.4 '
+                    'version and issue the following command on admin '
+                    'database:\n db.runCommand({ setParameter: 1, '
+                    'textSearchEnabled: true })')
+            else:
+                db.posts.ensure_index([('plain_content', 'text')])
         if opts.static_url_prefix:
             settings['static_url_prefix'] = opts.static_url_prefix
         if opts.twitter_consumer_key and opts.twitter_consumer_secret:
