281021870e1b4dc710716a8560bbcd447f6509ee
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-04-04 12:34:16 -0500

    Removing spaces

--- selene/helpers/avatar_helper.py
+++ selene/helpers/avatar_helper.py
@@ -3,7 +3,7 @@ import hashlib
 
 
 class Gravatar(object):
-    
+
     def get_avatar(self, email, size=48):
         md5email = hashlib.md5(email).hexdigest()
         query = "%s?s=%s" % (md5email, size)
