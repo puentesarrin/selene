0d1a359a831fa33dab90a7139f20954600b9b0dd
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-04-09 13:03:52 -0500

    Wrapper for better reading of handler request arguments

--- selene/forms/__init__.py
+++ selene/forms/__init__.py
@@ -11,10 +11,44 @@ class BaseForm(Form):
         self._locale_code = locale_code
         super(BaseForm, self).__init__(formdata, obj, prefix, **kwargs)
 
+    def process(self, formdata=None, obj=None, **kwargs):
+        if formdata is not None and not hasattr(formdata, 'getlist'):
+            formdata = TornadoArgumentsWrapper(formdata)
+        super(Form, self).process(formdata, obj, **kwargs)
+
     def _get_translations(self):
         if not hasattr(self, '_locale_code'):
             self._locale_code = 'en_US'
         return TornadoLocaleWrapper(self._locale_code)
+
+
+class TornadoArgumentsWrapper(dict):
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError:
+            raise AttributeError
+
+    def getlist(self, key):
+        try:
+            values = []
+            for v in self[key]:
+                v = to_unicode(v)
+                if isinstance(v, unicode):
+                    v = re.sub(r"[\x00-\x08\x0e-\x1f]", " ", v)
+                values.append(v)
+            return values
+        except KeyError:
+            raise AttributeError
 
 
 class TornadoLocaleWrapper(object):
