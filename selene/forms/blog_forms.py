fd6f423ff7fe28c0e67b1abcf3e76d1705d99a59
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-04-11 23:22:37 -0500

    Improving NewPostHandler #17

--- selene/forms/blog_forms.py
+++ selene/forms/blog_forms.py
@@ -1,32 +1,39 @@
 # -*- coding: utf-8 *-*
-from selene import options
+from selene import constants, options
 from selene.forms import BaseForm
 from wtforms import (TextField, BooleanField, TextAreaField, RadioField,
                      SelectField)
 from wtforms.validators import Required, Email
 
 
-class NewPostForm(BaseForm):
+class PostForm(BaseForm):
 
-    def __init__(self, formdata=None, obj=None, prefix='', **kwargs):
-        super(NewPostForm, self).__init__(formdata, obj, prefix, **kwargs)
+    def __init__(self, formdata=None, obj=None, prefix='', locale_code='en_US',
+        status_choices=[], text_type_choices=[], **kwargs):
+        super(PostForm, self).__init__(formdata, obj, prefix,
+            locale_code=locale_code, **kwargs)
         self.status.choices = options.STATUSES
         self.text_type.choices = options.get_allowed_text_types()
 
-    title = TextField(validators=[Required()])
+    title = TextField(validators=[Required(constants.TITLE_IS_REQUIRED)])
     slug = BooleanField()
     custom_slug = TextField()
-    tags = TextField(validators=[Required()])
-    content = TextAreaField(validators=[Required()])
-    status = RadioField(validators=[Required()])
-    text_type = SelectField(validators=[Required()])
+    tags = TextField(validators=[Required(constants.TAGS_ARE_REQUIRED)])
+    content = TextAreaField(validators=[
+        Required(constants.CONTENT_IS_REQUIRED)])
+    status = RadioField(validators=[Required(constants.STATUS_IS_REQUIRED)],
+        default='published')
+    text_type = SelectField(validators=[
+        Required(constants.TEXT_TYPE_IS_REQUIRED)])
 
 
 class NewCommentForm(BaseForm):
 
-    name = TextField(validators=[Required()])
-    email = TextField(validators=[Required(), Email()])
-    content = TextAreaField(validators=[Required()])
+    name = TextField(validators=[Required(constants.NAME_IS_REQUIRED)])
+    email = TextField(validators=[Required(constants.EMAIL_IS_REQUIRED),
+        Email(constants.EMAIL_IS_INVALID)])
+    content = TextAreaField(
+        validators=[Required(constants.CONTENT_IS_REQUIRED)])
 
 
 class SearchForm(BaseForm):
