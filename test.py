2fd8eb8c6a34a483090d55d50a0daf6a371a03cb
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-04-15 01:02:37 -0500

    Improving tests

--- test.py
+++ test.py
@@ -5,9 +5,4 @@ import unittest
 from test import *
 
 if __name__ == '__main__':
-    tornado.locale.LOCALE_NAMES['zh_HK'] = {
-        'name_en': 'Chinese (Hong Kong)',
-        'name': '\u4e2d\u6587(\u7e41\u9ad4)'
-    }
-    tornado.locale.load_translations("translations")
     unittest.main()
--- test/test_forms.py
+++ test/test_forms.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 *-*
+import tornado.locale
 import unittest
 
 from selene import forms
@@ -7,7 +8,11 @@ from selene import forms
 class TestRegisterForm(unittest.TestCase):
 
     def setUp(self):
-        pass
+        tornado.locale.LOCALE_NAMES['zh_HK'] = {
+            'name_en': 'Chinese (Hong Kong)',
+            'name': '\u4e2d\u6587(\u7e41\u9ad4)'
+        }
+        tornado.locale.load_translations("translations")
 
     def test_successfully(self):
         form = forms.RegisterForm(locale_code='en_US', **{
@@ -48,4 +53,12 @@ class TestRegisterForm(unittest.TestCase):
 
     def test_spanish_locale(self):
         form = forms.RegisterForm(locale_code='es_ES')
-        self.assertEqual(form.full_name.label.text, 'Nombre completo')
+        self.assertEqual(form.full_name.label.text, u'Nombre completo')
+        self.assertEqual(form.email.label.text, u'Correo electrónico')
+        self.assertEqual(form.password.label.text, u'Contraseña')
+
+    def test_chinese_locale_zh_CN(self):
+        form = forms.RegisterForm(locale_code='zh_CN')
+        self.assertEqual(form.full_name.label.text, u'完整姓名')
+        self.assertEqual(form.email.label.text, u'电子邮箱')
+        self.assertEqual(form.password.label.text, u'密码')
