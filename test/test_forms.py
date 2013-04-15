d66aa823a5eb992e32caf73947c1714c40125791
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-04-14 23:10:56 -0500

    More register form tests

--- test/test_forms.py
+++ test/test_forms.py
@@ -9,6 +9,14 @@ class TestRegisterForm(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_successfully(self):
+        form = forms.RegisterForm(locale_code='en_US', **{
+            'full_name': 'Gokú',
+            'email': 'goku@dragonball.gt',
+            'password': 'Nube Kinto'
+        })
+        self.assertEqual(form.validate(), True)
+
     def test_email_format(self):
         form = forms.RegisterForm(locale_code='en_US', **{
             'full_name': 'Gokú',
@@ -17,10 +25,24 @@ class TestRegisterForm(unittest.TestCase):
         })
         self.assertEqual(form.validate(), False)
 
-    def test_missing_fullname(self):
+    def test_required_fullname(self):
         form = forms.RegisterForm(locale_code='en_US', **{
             'email': 'goku@dragonball.gt',
             'password': 'Nube Kinto'
+        })
+        self.assertEqual(form.validate(), False)
+
+    def test_required_email(self):
+        form = forms.RegisterForm(locale_code='en_US', **{
+            'full_name': 'Gokú',
+            'password': 'Nube Kinto'
+        })
+        self.assertEqual(form.validate(), False)
+
+    def test_required_password(self):
+        form = forms.RegisterForm(locale_code='en_US', **{
+            'full_name': 'Gokú',
+            'email': 'goku@dragonball.gt',
         })
         self.assertEqual(form.validate(), False)
 
