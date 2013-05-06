cbb3b43192f8165a6ccbdf3a4bafad328a0fdcc5
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-05-06 16:38:15 -0500

    Removing deprecated url_helper

--- selene/helpers/__init__.py
+++ selene/helpers/__init__.py
@@ -2,5 +2,4 @@
 from selene.helpers.list_helper import *
 from selene.helpers.string_helper import *
 from selene.helpers.random_helper import *
-from selene.helpers.url_helper import *
 from selene.helpers.avatar_helper import *
