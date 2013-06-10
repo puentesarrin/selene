0e0d9eb52f631639ccdac93da35e65d4289de595
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-06-09 22:50:42 -0400

    Configure logging before to connect to database.

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ from selene import log, options, Selene, web
 
 if __name__ == '__main__':
     options.setup_options('selene.conf')
+    if opts.logging_db:
+        log.configure_mongolog()
     if not opts.db_rs:
         db = motor.MotorClient(opts.db_uri).open_sync()[opts.db_name]
         logging.info('Connected to a MongoDB standalone instance.')
@@ -18,8 +20,6 @@ if __name__ == '__main__':
         db = motor.MotorReplicaSetClient(opts.db_uri,
             replicaSet=opts.db_rs_name).open_sync()[opts.db_name]
         logging.info('Connected to a MongoDB replica set.')
-    if opts.logging_db:
-        log.configure_mongolog()
     http_server = tornado.httpserver.HTTPServer(Selene(db))
     tornado.web.ErrorHandler = web.ErrorHandler
     http_server.listen(opts.port)
