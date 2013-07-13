3e9f60d4904777cebc1cfe394ea2a09ece335f23
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-07-13 08:28:10 -0400

    Comment.

--- selene/log.py
+++ selene/log.py
@@ -6,6 +6,9 @@ from tornado.options import options
 
 
 def configure_mongolog():
+    """This method use a sync connection via PyMongo because the Python's
+    logging module is blocking. However we set the write concern to 0 for no
+    expecting a result."""
     client = pymongo.MongoClient(options.logging_db_uri, w=0)
 
     from mongolog.handlers import MongoHandler
